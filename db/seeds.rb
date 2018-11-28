0e06e7fa287458bd0fff729de8967611b5c853a2
henri clérisse <henri.clerisse@gmail.com>
2018-11-28 17:35:29 +0100

    Initial commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Flat.destroy_all
+
+puts 'Creating flats...'
+
+Flat.create!(
+  name: 'Le top tout simplement',
+  address: '67 rue de la chance Paris 75006',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Venez chez régis !',
+  address: '10 downing street London JSPLCP',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 100,
+  number_of_guests: 1
+)
+
+Flat.create!(
+  name: 'Super chambre avec vu sur le phare',
+  address: '10 avenue Saint-André Lille 59000',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 20,
+  number_of_guests: 14
+)
+
+Flat.create!(
+  name: 'Toulouloulouloulou',
+  address: '78 avenue trompette Biarritz 64200',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 20,
+  number_of_guests: 14
+)
+
+puts 'Finished!'
